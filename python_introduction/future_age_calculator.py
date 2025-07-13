5e655489f244c350bc2f9624439aa93cf8ca4f8c
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-07-13 05:21:50 -0700

    Adding file for  practicing receiving user input in python

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,9 @@
+
+user_current_age=int(input("How old are you? "))
+current_year = 2023
+future_year = 2050
+print(f"In {future_year}, you will be {user_current_age+ (future_year - current_year)} years old.")
+
+
+
+
