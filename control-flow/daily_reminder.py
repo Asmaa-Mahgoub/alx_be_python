fc95f753045ae65e681e60558163866769a96ffe
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-07-19 16:51:19 -0700

    Adding a new file for Personal Daily Reminder modified

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,17 +4,14 @@ time_bound = input("Is it time-bound? (yes/no):")
 
 match priority :
     case "high" :
-        if priority == "high" and time_bound == "yes" :
-            print(f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!")
-        elif priority == "high" and time_bound == "no" :
-            print(f"Note: '{task} is a {priority} priority task. Consider completing it when you have free time.")
+            message = f"'{task}' is a {priority} priority task"
     case "medium" :
-        if priority == "medium" and time_bound == "yes" :
-            print(f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!")
-        elif priority == "medium" and time_bound == "no" :
-            print(f"Note: '{task} is a {priority} priority task. Consider completing it when you have free time.")
+            message = f"'{task}' is a {priority} priority task "
     case "low" :
-        if priority == "low" and time_bound == "yes" :
-            print(f"Reminder: '{task} is a {priority} priority task that requires immediate attention today!")
-        elif priority == "low" and time_bound == "no" :
-            print(f"Note: '{task} is a {priority} priority task. Consider completing it when you have free time.")
+            message = f"'{task}' is a {priority} priority task "
+
+if time_bound == "yes":
+       message = "Reminder: " + message + "that requires immediate attention today!"
+else :
+       message = "Note: " + message + "Consider completing it when you have free time." 
+print(message)  
