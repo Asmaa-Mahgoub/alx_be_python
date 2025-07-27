250162d8746473377ea94be3a9dfede86a91d74f
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-07-27 14:56:44 -0700

    create a futuredate from current date2

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,14 +2,18 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    print(f"Current date is {current_date}")
-    number_of_days = int(input("Please enter a number of days (as an integer)"))
+    formatted_current = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date is {formatted_current}")
+
+    number_of_days = int(input("Please enter a number of days (as an integer): "))
 
     def calculate_future_date():
-        td = timedelta(days=number_of_days)  # Use 'days' keyword argument
-        future_date = current_date + td      # Add timedelta to datetime
-        print(f"The future date is {future_date}")
+        td = timedelta(days=number_of_days)
+        future_date = current_date + td
+        formatted_future = future_date.strftime("%Y-%m-%d")
+        print(f"The future date is {formatted_future}")
 
     calculate_future_date()
 
 display_current_datetime()
+
