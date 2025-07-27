b7ff80bc6083d10af301650801309f12fbc7a4c7
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-07-27 15:30:03 -0700

    create a temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,13 +1,13 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)-32
-CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)+32
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 
 
 def convert_to_celsius(fahrenheit):
-    result = (fahrenheit* FAHRENHEIT_TO_CELSIUS_FACTOR)
+    result = (fahrenheit* FAHRENHEIT_TO_CELSIUS_FACTOR)-32
     print(result)
 
 def convert_to_fahrenheit(celsius):
-    result = (celsius* CELSIUS_TO_FAHRENHEIT_FACTOR)
+    result = (celsius* CELSIUS_TO_FAHRENHEIT_FACTOR)+32
     print(result)
 
 user_input = float(input("Enter the temperature to convert: "))
@@ -17,8 +17,8 @@ celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/
 #100.0°F is 37.77777777777778°C
 
 if celsius_or_fahrenheit == "f":
-    temp = user_input* FAHRENHEIT_TO_CELSIUS_FACTOR
+    temp = user_input* FAHRENHEIT_TO_CELSIUS_FACTOR-32
     print(f"{user_input}°F is {temp}°C")
 else:
-    temp = user_input* CELSIUS_TO_FAHRENHEIT_FACTOR
+    temp = user_input* CELSIUS_TO_FAHRENHEIT_FACTOR+32
     print(f"{user_input}°C is {temp}°F")
