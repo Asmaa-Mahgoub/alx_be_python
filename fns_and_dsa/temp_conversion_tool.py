411b3d8792a76960c954cc958fee2d7d62676243
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-07-27 15:41:57 -0700

    create a temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -12,14 +12,13 @@ def convert_to_fahrenheit(celsius):
 
 user_input = float(input("Enter the temperature to convert: "))
 celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().lower()
-#Enter the temperature to convert: 100
-#Is this temperature in Celsius or Fahrenheit? (C/F): F
-#100.0°F is 37.77777777777778°C
 
 if celsius_or_fahrenheit == "f":
     temp = (user_input - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     print(f"{user_input}°F is {temp}°C")
-else:
+elif celsius_or_fahrenheit == "c":
     temp = (user_input* CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     print(f"{user_input}°C is {temp}°F")
+else:
+    print("“Invalid temperature. Please enter a numeric value.")
 
