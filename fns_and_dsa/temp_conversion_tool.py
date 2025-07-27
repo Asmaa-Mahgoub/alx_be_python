e75d493010e942561287e1fd7bffbe08c0aa8f80
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-07-27 15:37:48 -0700

    create a temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,12 +3,12 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 
 def convert_to_celsius(fahrenheit):
-    FAHRENHEIT_TO_CELSIUS = (fahrenheit* FAHRENHEIT_TO_CELSIUS_FACTOR-32)
-    print(FAHRENHEIT_TO_CELSIUS)
+    result = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    print(result)
 
 def convert_to_fahrenheit(celsius):
-    CELSIUS_TO_FAHRENHEIT = (celsius* CELSIUS_TO_FAHRENHEIT_FACTOR+32)
-    print(CELSIUS_TO_FAHRENHEIT)
+    result = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    print(result)
 
 user_input = float(input("Enter the temperature to convert: "))
 celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().lower()
@@ -17,8 +17,9 @@ celsius_or_fahrenheit = input("Is this temperature in Celsius or Fahrenheit? (C/
 #100.0°F is 37.77777777777778°C
 
 if celsius_or_fahrenheit == "f":
-    temp = user_input* FAHRENHEIT_TO_CELSIUS_FACTOR-32
+    temp = (user_input - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     print(f"{user_input}°F is {temp}°C")
 else:
-    temp = user_input* CELSIUS_TO_FAHRENHEIT_FACTOR+32
+    temp = (user_input* CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     print(f"{user_input}°C is {temp}°F")
+
