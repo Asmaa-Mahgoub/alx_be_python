5f312426e052eb05b325cd817cb8b1aadec1cf6b
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-08-03 06:58:20 -0700

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -19,7 +19,7 @@ class BankAccount:
             return False
 
     def display_balance(self):
-        print(f"Current Balance: ${float(self.account_balance)}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
 
 #CIB= BankAccount(43000)
 #CIB.deposit(67)
