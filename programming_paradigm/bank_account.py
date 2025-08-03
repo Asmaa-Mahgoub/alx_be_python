2d32d936513bab24bf4805264373ae5d9be51455
Asmaa Mahgoub <asmaamahgoub262@gmail.com>
2025-08-03 06:50:30 -0700

    Create a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,26 +6,26 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
+        #print(f"Deposited: ${amount}")
 
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            print(f"Withdraw: ${amount}")
+            #print(f"Withdraw: ${amount}")
             return True
             
         else:
-            print("Insufficient funds")
+            #print("Insufficient funds")
             return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
 
-CIB= BankAccount(43000)
-#CIB.deposit(103000)
+#CIB= BankAccount(43000)
+#CIB.deposit(67)
 #CIB.withdraw(50)
-CIB.withdraw(100000) 
-CIB.display_balance()
+#CIB.withdraw(100000) 
+#CIB.display_balance()
 
 
 
